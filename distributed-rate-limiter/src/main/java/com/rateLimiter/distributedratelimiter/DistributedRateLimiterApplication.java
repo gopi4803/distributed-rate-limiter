ed3df1a20d3bf59e146851ea8a8cf7e44e42d1a8
gopi4803 <chandugc48@gmail.com>
2026-06-25 16:25:49 +0530

    feat:add core rate limiting abstractions and clock provider

--- distributed-rate-limiter/src/main/java/com/rateLimiter/distributedratelimiter/DistributedRateLimiterApplication.java
+++ distributed-rate-limiter/src/main/java/com/rateLimiter/distributedratelimiter/DistributedRateLimiterApplication.java
@@ -1,4 +1,4 @@
-package com.rateLimiter.distributedRateLimiter;
+package com.rateLimiter.distributedratelimiter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
