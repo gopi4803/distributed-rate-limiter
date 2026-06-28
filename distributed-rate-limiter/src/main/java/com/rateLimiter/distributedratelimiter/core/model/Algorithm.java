61ba60853dd0d58c273f93ef1aded5125b1b2168
gopi4803 <chandugc48@gmail.com>
2026-06-28 23:47:08 +0530

    Refactor: Abstraction of Rate Limiter

--- distributed-rate-limiter/src/main/java/com/rateLimiter/distributedratelimiter/core/model/Algorithm.java
+++ distributed-rate-limiter/src/main/java/com/rateLimiter/distributedratelimiter/core/model/Algorithm.java
@@ -1,6 +1,7 @@
 package com.rateLimiter.distributedratelimiter.core.model;
 
 public enum Algorithm {
+    FIXED_WINDOW,
     TOKEN_BUCKET,
     SLIDING_WINDOW_COUNTER
 }
--- distributed-rate-limiter/src/main/java/com/rateLimiter/distributedratelimiter/redis/RedisLuaSlidingWindowLimiter.java
+++ distributed-rate-limiter/src/main/java/com/rateLimiter/distributedratelimiter/redis/RedisLuaSlidingWindowLimiter.java
@@ -1,45 +1,31 @@
 package com.rateLimiter.distributedratelimiter.redis;
 
-import com.rateLimiter.distributedratelimiter.core.RateLimiter;
 import com.rateLimiter.distributedratelimiter.core.model.RateLimitResult;
 import com.rateLimiter.distributedratelimiter.core.model.RateLimitRule;
 import com.rateLimiter.distributedratelimiter.core.utils.ValidationUtils;
-import com.rateLimiter.distributedratelimiter.exceptions.InvalidLuaScriptException;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.script.RedisScript;
 
 import java.util.List;
-import java.util.Objects;
 
-public class RedisLuaSlidingWindowLimiter implements RateLimiter {
+public class RedisLuaSlidingWindowLimiter extends AbstractRedisLuaRateLimiter {
 
-    private final StringRedisTemplate redisTemplate;
-    private final RedisScript<List<Long>> script;
-
-    public RedisLuaSlidingWindowLimiter(StringRedisTemplate redisTemplate, RedisScript<List<Long>> script) {
-        this.redisTemplate= Objects.requireNonNull(redisTemplate,"RedisTemplate must be non null");
-        this.script=Objects.requireNonNull(script,"Script must be non null");
+    public RedisLuaSlidingWindowLimiter(
+            StringRedisTemplate redisTemplate,
+            RedisScript<List<Long>> script) {
+        super(redisTemplate, script);
     }
 
     @Override
-    public RateLimitResult tryAcquire(String key, RateLimitRule rule) {
+    public RateLimitResult tryAcquire(
+            String key,
+            RateLimitRule rule) {
         ValidationUtils.validateInputs(key, rule);
-        List<Long> result =
-                redisTemplate.execute(
-                        script,
-                        List.of(
-                                RedisKeyGenerator
-                                        .generateWindowKey(
-                                                key)),
-                        String.valueOf(
-                                rule.limit()),
-                        String.valueOf(
-                                rule.window().toMillis()));
-
-        if (result == null || result.size() != 3) {
-            throw new InvalidLuaScriptException("Unexpected Lua response");
-        }
-
-        return new RateLimitResult(result.get(0) == 1L, result.get(1), result.get(2));
+        String redisKey = RedisKeyGenerator.generateWindowKey(key);
+        List<Long> result = executeScript(List.of(redisKey),String.valueOf(rule.limit()),String.valueOf(rule.window().toMillis()));
+        return new RateLimitResult(
+                result.get(0) == 1L,
+                result.get(1),
+                result.get(2));
     }
 }
